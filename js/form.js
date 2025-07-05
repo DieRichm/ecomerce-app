22b26ba605696de331ccbb53dc5c67af9273f97d
DieRichm <boadis334@gmail.com>
2025-07-05 12:18:28 +0000

    done

---
+++ js/form.js
@@ -0,0 +1,33 @@
+// let send = document.getElementById("send")
+// let form = document.getElementById("form")
+
+// console.log(send)
+let cover = document.getElementById("cover")
+cover.onclick=function(){
+
+
+    cover.src="images/IMG-20250621-WA0128.jpg";
+
+ 
+    
+
+
+
+    // setInterval(()=>{
+    //      cover.src="images/mmgif.gif";
+
+
+    // })
+    // setTimeout(() => {
+    //     images/Untitled.jpg
+        
+    // },  1000);
+
+}
+
+
+
+
+
+
+
---
+++ js/myscript.js
@@ -0,0 +1,96 @@
+// // // // // // // //  let CupOfGlass="Micky";
+// // // // // // // //  let PlasticCup="Orange Juice";
+// // // // // // // //  let Cup = CupOfGlass + PlasticCup ;
+// // // // // // // //  let z = Cup ;
+// // // // // // // // console.log(z);
+// // // // // // // // alert(z);
+// // // // // // // // document.getElementById("code").innerHTML = z
+// // // // // // // // document.getElementById("coder").innerHTML = z
+
+
+// // let MyI="I ";
+// // let MyAm="Am ";
+// // let MyCoding="Coding ";
+// // let MyOut="Out ";
+// // let MyHere="Here ";
+
+// // // // // // // // let My= MyI + MyAm + MyCoding + MyOut + MyHere
+// // // // // // // // let z = My;
+// // // // // // // // alert(z);
+// // // // // // // // console.log(z);
+
+
+
+// // // // // // // let MyFistNumber =2000
+// // // // // // // let MySecondNumber=5000
+// // // // // // // let MyThirdNumber=600
+
+// // // // // // // let Sub = MyFistNumber- MySecondNumber - MyThirdNumber;
+// // // // // // // let Add = MyFistNumber + MySecondNumber + MyThirdNumber;
+// // // // // // // let Div = MyFistNumber/ MySecondNumber/ MyThirdNumber;
+ 
+// // // // // // // console.log(Sub)
+// // // // // // // console.log(Add)
+// // // // // // // console.log(Div)
+
+// // // // // // // let test = MyFistNumber==MySecondNumber
+// // // // // // // let test2 = MyFistNumber===MySecondNumber
+// // // // // // // test ? console.log( "They are equal"):console.log("They are not equal")
+// // // // // // // test ? console.log("They are not equal"):console.log("They are equal")
+// // // // // // // console.log(test)
+
+
+// // // // // // // let Number = 100
+// // // // // // // let EvenOrOdd = Number % 2
+// // // // // // // console.log(EvenOrOdd)
+
+
+
+
+// // // // let start = 10 ;
+// // // //  start +=17 // start = start + 17
+// // // //  start -=70 // start = start -70
+// // // //  console.log(start)
+
+// // // // // for (let start=0; >start<=10;start++){
+// // // // //     console.log(start)
+// // // // // }
+
+ 
+
+
+// let dispaly = document.getElementById("textdisplay");
+// let Mubarack = 100;
+// let Boadi = 200;
+// let Asanti = 300;
+
+// let fist = Mubarack > Boadi && Mubarack > Asanti;
+
+// let second = Boadi > Mubarack && Boadi > Asanti;
+
+// let third = Asanti > Mubarack && Asanti > Boadi;
+ 
+// fist ? dispaly.innerHTML = " Mubarack Has The Biggest Number" : console. log("")
+// second ? dispaly.innerHTML = "Boadi Has The Biggest Number" : console. log("")
+// third ? dispaly.innerHTML = " Asant Has The Biggest Number"  : console. log("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
