1f84f8943cc5ebbb892a8ffbdabf84f119828ed1
DieRichm <boadis334@gmail.com>
2025-07-06 13:20:30 +0000

    jjjjjj

--- js/form.js
+++ js/form.js
@@ -1,29 +1,348 @@
-// let send = document.getElementById("send")
-// let form = document.getElementById("form")
+// // let send = document.getElementById("send")
+// // let form = document.getElementById("form")
 
-// console.log(send)
-let cover = document.getElementById("cover")
-cover.onclick=function(){
+// // console.log(send)
+// let cover = document.getElementById("cover")
+// cover.onclick=function(){
 
 
-    cover.src="images/IMG-20250621-WA0128.jpg";
+//     cover.src="images/IMG-20250621-WA0128.jpg";
+
+//     send . onclick = function ()
+
 
  
     
 
 
 
-    // setInterval(()=>{
-    //      cover.src="images/mmgif.gif";
+//     // setInterval(()=>{
+//     //      cover.src="images/mmgif.gif";
 
 
-    // })
-    // setTimeout(() => {
-    //     images/Untitled.jpg
+//     // })
+//     // setTimeout(() => {
+//     //     images/Untitled.jpg
         
-    // },  1000);
+//     // },  1000);
 
-}
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
